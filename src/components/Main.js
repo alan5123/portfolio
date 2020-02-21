8990651deb1ab39068eac06a4026780b7d66628e
alan5123 <alanparker5123@gmail.com>
2020-02-20 22:25:05 -0500

    bug fix

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,14 +4,15 @@ import styled from "styled-components"
 
 const Main = (props) => {
     return (
+        <div>
       <div className = 'FirstColumn'>
 
       </div>
 
       <div className = 'SecondColumn'>
-          
+          sdad
       </div>
-       
+      </div>
     );
   };
   
